45fd3eb4a3a8a9c213cfb55754f074a932f86261
Aascharya paudel <153407432+Mangouh7@users.noreply.github.com>
2025-12-06 15:59:04 +0545

    Update Spotify redirect URI for GitHub Pages

--- js/spotify-auth.js
+++ js/spotify-auth.js
@@ -4,7 +4,7 @@
 
 const SPOTIFY_CONFIG = {
     clientId: 'ef640275d3494247b4c9d3913de0c82d',
-    redirectUri: 'http://localhost:8080/',
+    redirectUri: 'https://mangouh7.github.io/personal-website/',
     scopes: 'user-read-currently-playing user-read-playback-state'
 };
 
